60d3ef93e4cbd5087885e84ffb2fd84448cc3586
LJZ458 <104240462+LJZ458@users.noreply.github.com>
2024-02-08 12:07:14 -0600

    Add files via upload

--- decay_fit.py
+++ decay_fit.py
@@ -2,7 +2,7 @@ import numpy as np
 from sympy import *
 import matplotlib.pyplot as plt
 
-f=open('20Mg_new.txt',"r")
+f=open('20Mggate1.txt',"r")
 lines=f.readlines()
 raw=[]
 for x in lines:
@@ -14,49 +14,52 @@ data = []
 
 
 for item in raw:
-     
     data.append(item)
 
 for i in data:    
         decay_time.append(int((i[0][1:-1])))
-
         if len(i[-1])==1:
              decay_counts.append(int(i[-1]))
         else:
             decay_counts.append(int(i[-1][0:-1]))
 
 f.close()
-decay_counts = decay_counts[0:-1]
-# decay_counts   = [3487.0, 3422.0, 3118.0, 2871.0, 2813.0, 2560.0, 2500.0, 2256.0, 2175.0, 2013.0, 1934.0, 1779.0, 1715.0, 1614.0, 1472.0, 1450.0, 1337.0, 1249.0, 1207.0, 1202.0, 1053.0, 1064.0, 1029.0, 954.0, 874.0, 890.0, 884.0, 765.0, 756.0, 714.0, 663.0, 703.0, 638.0, 616.0, 562.0, 550.0, 535.0, 546.0, 534.0, 484.0, 479.0, 487.0, 442.0, 439.0, 408.0, 431.0, 414.0, 385.0, 374.0, 400.0, 371.0, 364.0, 343.0, 346.0, 323.0, 325.0, 318.0, 305.0, 284.0, 332.0, 255.0, 288.0, 249.0, 288.0, 256.0, 243.0, 243.0, 226.0, 276.0, 250.0, 233.0, 236.0, 219.0, 219.0, 223.0, 233.0, 214.0, 220.0, 242.0, 206.0, 202.0, 199.0, 191.0, 219.0, 190.0, 169.0, 153.0, 183.0, 172.0, 183.0, 202.0, 151.0, 184.0, 188.0, 161.0, 170.0, 178.0, 165.0, 140.0, 157.0, 150.0, 146.0, 133.0, 136.0, 139.0, 155.0, 140.0, 126.0, 149.0, 122.0, 122.0, 136.0, 122.0, 143.0, 118.0, 117.0, 115.0, 111.0, 126.0, 115.0, 112.0, 113.0, 129.0, 114.0, 96.0, 106.0, 94.0, 128.0, 111.0, 123.0, 113.0, 116.0, 112.0, 96.0, 104.0, 85.0, 87.0, 88.0, 101.0, 80.0, 81.0, 103.0, 90.0, 78.0, 94.0, 82.0, 86.0, 88.0, 81.0, 74.0, 81.0, 93.0, 70.0, 73.0, 69.0, 75.0, 86.0, 73.0, 68.0, 87.0, 75.0, 54.0, 79.0, 77.0, 65.0, 69.0, 74.0, 70.0, 64.0, 56.0, 60.0, 58.0, 61.0, 61.0, 44.0, 75.0, 52.0, 62.0, 65.0, 54.0, 62.0, 60.0, 72.0, 60.0, 54.0, 58.0, 53.0, 46.0, 48.0, 46.0, 60.0, 44.0, 54.0, 42.0, 41.0, 36.0, 31.0, 38.0, 45.0, 37.0, 43.0, 45.0, 38.0, 41.0, 40.0, 27.0, 43.0, 29.0, 55.0, 44.0, 28.0, 35.0, 37.0, 39.0, 43.0, 23.0, 45.0, 36.0, 35.0, 22.0, 34.0, 21.0, 36.0, 32.0, 31.0, 34.0, 20.0, 22.0, 27.0, 28.0, 26.0, 27.0, 35.0, 26.0, 22.0, 24.0, 27.0, 34.0, 27.0, 32.0, 27.0, 29.0, 25.0, 30.0, 29.0, 27.0, 27.0, 23.0, 25.0, 23.0]
+# a = np.loadtxt("Total3.dat")
+# decay_counts = np.transpose(a)[1]
+# # decay_counts = np.transpose(a)[0:300]
 
+# # print(decay_counts)
+# # decay_counts = decay_counts[0:-1]
 y_i = decay_counts
 
-
-
 ################################START TIME ----------------------END TIME################################################################
+bin_size = 0.008
+
 
 #linspace(start_time,endtime,number of bins)
-t_val = np.linspace(0.000,249*0.008,len(y_i))
-# ax3.plot(t_val,log(primary_fit_function_val(lambda_1_val_itr,t_val,intensity_1_val_itr)), color = "darkviolet", label = "Primary Beam"    )
+# t_val = []
+# for i in range(0,len(y_i)):
+#     t_val.append(bin_size*i)
+# t_val = np.array(t_val)
+t_val = np.linspace(bin_size/2, len(decay_counts)*bin_size+bin_size/2,len(decay_counts),endpoint = False)
 
-
+# print(t_val)
 ############################################################################################################################################################
 ################################  PARAMTERS FOR CURVE FITTING###############################################################################################
 
 
 Max_Iterations = 200
-tolerance = 1e-6
+tolerance = 0.000001
 decay_cons_20Na = np.log(2)/0.44790
-decay_cons_20Mg = np.log(2)/0.090
-
+decay_cons_20Mg = np.log(2)/0.075
+intensity_1_est = decay_counts[0]
 ####################################################iNITIAL GUESS PARAMETERS#############################################################################
 #IF THE YOU WANT TO FIX THE PARAMETER JUST ENTER 0 HERE AND REPLACE THE PARAMETER AS CONSTANT YOU WANT IT TO FIXED IN THE FUNCTION BELOW, THERE MIGHT BE ISSUE IF
 #YOU SPECIFIED THE PARAMETER HERE AS 0 BUT STILL INCLUDING IT IN THE FUNCTION DEFINED.
-lambda_1_val_init, lambda_2_val_init, lambda_3_val_init,intensity_1_val_init,intensity_2_val_init,intensity_3_val_init,backgrounds_val_init = decay_cons_20Mg,0,0,4000,0,0,1
+lambda_1_val_init, lambda_2_val_init, lambda_3_val_init,intensity_1_val_init,intensity_2_val_init,intensity_3_val_init,backgrounds_val_init = decay_cons_20Mg,0,0,intensity_1_est,0,0,0
 
 
 # decay fit using maximum log-likelyhood approach where parameters are found using algorithm based on Levenberg-Marquardt Method
-
 
 #compute chi square for maximum log likelihood:
 
@@ -67,8 +70,23 @@ def chi_square_ml(y_i,y_fit):
         return 0
     else:
         for i in range(0,len(y_i)):
-            value += y_fit[i]- y_i[i] + y_i[i]*np.log(y_i[i]/y_fit[i])
-        return value
+            if y_i[i] == 0.:
+                value += (y_fit[i])
+            else:
+                value += (y_fit[i]- y_i[i] + y_i[i]*np.log(y_i[i]/y_fit[i]))
+    return value*2
+
+def exp_chi_square(y_i,y_fit):
+    value = 0
+    for i in range(0,len(y_i)):
+            if y_i[i] < 4.2:
+                value = -2*y_i[i]*np.log(y_i[i]) + y_i[i]**2*np.log(4)  
+                value += np.absolute(y_fit[i])
+            else:
+                value += np.absolute(y_fit[i]- y_i[i] + y_i[i]*np.log(y_i[i]/y_fit[i]))
+    return value*2
+
+
 
 #partial derivative computations are done using external package by sympy, specifications can be foubd at https://docs.sympy.org/latest/tutorials/index.html#tutorials
 #initialization of variables 
@@ -79,12 +97,13 @@ init_printing(use_unicode=True)
 
 ##################################################create functions using the parameters initialized#########################################################
 ##################################IF THE PARAMETER YOU SPECIFIED IS FIXED AT A CONSTANT, DO NOT ENETER THE PARAMETER HERE, USE THE CONSTANT VALUE DIRECTLY#############
-y_fit_func =  intensity_1*exp(-lambda_1*t)+ 0.7*  (decay_cons_20Na/(decay_cons_20Na-lambda_1))*   intensity_1   *   ( exp(-lambda_1*t) -exp(-decay_cons_20Na*t)) + backgrounds
-
+y_fit_func =  intensity_1*exp(-lambda_1*t)+ 0.7*  (decay_cons_20Na/(decay_cons_20Na-lambda_1))*  intensity_1 *   ( exp(-lambda_1*t) -exp(-decay_cons_20Na*t))
+# y_fit_func =  intensity_1*bin_size*exp(-lambda_1*t) + backgrounds*bin_size
 #note by using the package, the above function can be printed but can not be used in calculations, in order to create a real function that returns value, need to use function lambdify to
 #reutrn a lambda like function as usual
 y_fit_func_val = lambdify([lambda_1, lambda_2, lambda_3,t,intensity_1,intensity_2,intensity_3,backgrounds],y_fit_func)
-gradient = diff(y_fit_func, t)
+gradient = diff(y_fit_func, lambda_1)
+print(gradient)
 gradient_val = lambdify([lambda_1, lambda_2, lambda_3,t,intensity_1,intensity_2,intensity_3,backgrounds],gradient)
 
 
@@ -136,14 +155,14 @@ def fit_function_val(lambda_1_val, lambda_2_val, lambda_3_val,t_val,intensity_1_
 def primary_fit_function_val(lambda_1_val,t_val,intensity_1_val):
     y_fit = []
     for i in range(0,len(t_val)):
-        y_fit.append(intensity_1_val*exp(-lambda_1_val*t_val[i]))
+        y_fit.append(intensity_1_val*lambda_1_val*exp(-lambda_1_val*t_val[i]))
     return y_fit
 
 
 def secondary_fit_function_val(lambda_1_val, lambda_2_val,t_val,intensity_1_val,intensity_2_val):
     y_fit = []
     for i in range(0,len(t_val)):
-        y_fit.append(0.7 *(lambda_2_val/(lambda_2_val-lambda_1_val))*   intensity_1_val   *   (exp(-lambda_1_val*t_val[i]) - exp(-lambda_2_val*t_val[i])) )
+        y_fit.append(0.7 *lambda_1_val*(lambda_2_val/(lambda_2_val-lambda_1_val))*   intensity_1_val   *   (exp(-lambda_1_val*t_val[i]) - exp(-lambda_2_val*t_val[i])) )
     return y_fit
 
 
@@ -169,14 +188,14 @@ def log_fit_function_val(lambda_1_val, lambda_2_val, lambda_3_val,t_val,intensit
 def log_primary_fit_function_val(lambda_1_val,t_val,intensity_1_val):
     y_fit = []
     for i in range(0,len(t_val)):
-        y_fit.append(np.log(float(intensity_1_val*exp(-lambda_1_val*t_val[i]))))
+        y_fit.append(np.log(float(intensity_1_val*lambda_1_val*exp(-lambda_1_val*t_val[i]))))
     return y_fit
 
 
 def log_secondary_fit_function_val(lambda_1_val, lambda_2_val,t_val,intensity_1_val,intensity_2_val):
     y_fit = []
     for i in range(0,len(t_val)):
-        y_fit.append(np.log(float(0.7 *(lambda_2_val/(lambda_2_val-lambda_1_val))*   intensity_1_val   *   (exp(-lambda_1_val*t_val[i]) - exp(-lambda_2_val*t_val[i])) )))
+        y_fit.append(np.log(float(0.7*lambda_1_val *(lambda_2_val/(lambda_2_val-lambda_1_val))*   intensity_1_val   *   (exp(-lambda_1_val*t_val[i]) - exp(-lambda_2_val*t_val[i])) )))
     return y_fit
 
 
@@ -193,7 +212,9 @@ def extrm_vector(y_i,lambda_1_val,lambda_2_val,lambda_3_val,intensity_1_val,inte
     dy_di_1_val =  dy_di_1(lambda_1_val, lambda_2_val, lambda_3_val,t_val,intensity_1_val,intensity_2_val,intensity_3_val,backgrounds_val)
     dy_di_2_val = dy_di_2(lambda_1_val, lambda_2_val, lambda_3_val,t_val,intensity_1_val,intensity_2_val,intensity_3_val,backgrounds_val)
     dy_di_3_val = dy_di_3(lambda_1_val, lambda_2_val, lambda_3_val,t_val,intensity_1_val,intensity_2_val,intensity_3_val,backgrounds_val)
-    dy_dbackgrounds_val =  np.ones(len(t_val))*dy_dbackgrounds(lambda_1_val, lambda_2_val, lambda_3_val,t_val,intensity_1_val,intensity_2_val,intensity_3_val,backgrounds_val)
+    dy_dbackgrounds_val =  dy_dbackgrounds(lambda_1_val, lambda_2_val, lambda_3_val,t_val,intensity_1_val,intensity_2_val,intensity_3_val,backgrounds_val)
+    # dy_dbackgrounds_val =  dy_dbackgrounds(lambda_1_val, lambda_2_val, lambda_3_val,t_val,intensity_1_val,intensity_2_val,intensity_3_val,backgrounds_val)
+
     # print(type(dy_dlambda_1_val))
     if lambda_2_val_init==0 or  type(dy_dlambda_2_val) != np.ndarray :
         dy_dlambda_2_val = np.zeros(len(t_val))
@@ -206,7 +227,10 @@ def extrm_vector(y_i,lambda_1_val,lambda_2_val,lambda_3_val,intensity_1_val,inte
 
     if backgrounds_val_init == 0:
         dy_dbackgrounds_val =  np.zeros(len(t_val))
+    elif backgrounds_val_init != 0 and type(dy_dbackgrounds) != np.ndarray:
+        dy_dbackgrounds_val =  np.ones(len(t_val))*dy_dbackgrounds(lambda_1_val, lambda_2_val, lambda_3_val,t_val,intensity_1_val,intensity_2_val,intensity_3_val,backgrounds_val)
 
+    
     val = 0
     for i in range(0,len(y_i)):
         val += (y_i[i] - y_fit[i])/y_fit[i] * dy_dlambda_1_val[i]
@@ -255,9 +279,9 @@ def extrm_vector(y_i,lambda_1_val,lambda_2_val,lambda_3_val,intensity_1_val,inte
     
     new_vector = []
     for i in range(0,len(test)):
-        if test[i] != 0:
+        if test[i] != 0.:
             new_vector.append(vector[i])
-    
+
     return new_vector
 
 
@@ -307,10 +331,12 @@ def curvature_matrix(lambda_1_val,lambda_2_val,lambda_3_val,intensity_1_val,inte
         dy_di_3_val = dy_di_3(lambda_1_val, lambda_2_val, lambda_3_val,t_val,intensity_1_val,intensity_2_val,intensity_3_val,backgrounds_val)
         if type(dy_di_3_val) != np.ndarray:
             dy_di_3_val = np.zeros(len(t_val))
-    if backgrounds_val_init == 0:
+    dy_dbackgrounds_val =  dy_dbackgrounds(lambda_1_val, lambda_2_val, lambda_3_val,t_val,intensity_1_val,intensity_2_val,intensity_3_val,backgrounds_val)
+
+    if backgrounds_val_init==0:
         dy_dbackgrounds_val =  np.zeros(len(t_val))
-    else:
-        dy_dbackgrounds_val = np.ones(len(t_val))*dy_dbackgrounds(lambda_1_val, lambda_2_val, lambda_3_val,t_val,intensity_1_val,intensity_2_val,intensity_3_val,backgrounds_val)
+    elif backgrounds_val_init!=0 and type(dy_dbackgrounds) != np.ndarray:
+        dy_dbackgrounds_val =  np.ones(len(t_val))*dy_dbackgrounds(lambda_1_val, lambda_2_val, lambda_3_val,t_val,intensity_1_val,intensity_2_val,intensity_3_val,backgrounds_val)
 
 #-----------------------------------------------row-1-compute--------------------------------------------------------------------------------------
 #--------------------------------------------------------------------------------------------------------------------------------------------------
@@ -541,7 +567,8 @@ def curvature_prime(curvature_matrix,lambda_val):
 
 
 def STEPPING_VEC(curvature_matrix,extremum_vector):
-    step_vec = np.matmul(np.linalg.inv(curvature_matrix),np.transpose(np.array(extremum_vector)))
+    step_vec = np.matmul(np.linalg.inv(curvature_matrix),np.array(extremum_vector))
+    print(step_vec)
     return step_vec
 #save the copy of parameter initialized by creating new set of parameters for iterations
 lambda_1_val_itr, lambda_2_val_itr, lambda_3_val_itr,intensity_1_val_itr,intensity_2_val_itr,intensity_3_val_itr,backgrounds_val_itr = lambda_1_val_init,lambda_2_val_init,lambda_3_val_init,intensity_1_val_init,intensity_2_val_init,intensity_3_val_init,backgrounds_val_init
@@ -570,17 +597,15 @@ lambda_val = 0.0001
 
 
 for i in range(0,Max_Iterations):
+    print("half life for primary beam is: ", np.log(2)/lambda_1_val_itr  )
     backup = [lambda_1_val_itr, lambda_2_val_itr, lambda_3_val_itr,intensity_1_val_itr,intensity_2_val_itr,intensity_3_val_itr,backgrounds_val_itr]
     chi2 = chi_square_ml(y_i,fit_function_val(lambda_1_val_itr, lambda_2_val_itr, lambda_3_val_itr,t_val,intensity_1_val_itr,intensity_2_val_itr,intensity_3_val_itr,backgrounds_val_itr))
-    print("iteration",i," Chi square is:", chi2[0]/(len(t_val)))
+    print("iteration",i," Chi square is:", chi2[0]/(len(t_val)-(np.count_nonzero(param_list_init)  )))
     curvature = curvature_matrix(lambda_1_val_itr,lambda_2_val_itr,lambda_3_val_itr,intensity_1_val_itr,intensity_2_val_itr,intensity_3_val_itr,backgrounds_val_itr,t_val)
     curvature_new = curvature_prime(curvature,lambda_val)
-    # print(curvature_new)
-    # print(curvature)
     etxrm = extrm_vector(y_i,lambda_1_val_itr,lambda_2_val_itr,lambda_3_val_itr,intensity_1_val_itr,intensity_2_val_itr,intensity_3_val_itr,backgrounds_val_itr,t_val) 
     step_vec = STEPPING_VEC(curvature_new,etxrm)
     step_vec_new = [0,0,0,0,0,0,0]
-    # print(curvature)
     j=0
     for i in range(0,len(param_list_init)):
         if param_list_init[i] == 0:
@@ -590,23 +615,32 @@ for i in range(0,Max_Iterations):
     # print(step_vec_new)
     param_list_backup = param_list_itr
     param_list_itr = np.add(np.array(step_vec_new),np.array(param_list_itr))
+    param_diff = param_list_itr-param_list_backup
     chi2_new = chi_square_ml(y_i,fit_function_val(param_list_itr[0], param_list_itr[1], param_list_itr[2],t_val,param_list_itr[3],param_list_itr[4],param_list_itr[5],param_list_itr[6]))
-    if np.absolute(chi2_new[0]-chi2[0]) < tolerance:
-        break
 
     if chi2_new[0] < chi2[0]:
+        if np.sum(np.absolute(param_diff)) <= tolerance:
+            param_list_itr = param_list_backup
+            print("chi_new smaller than chi_2, difference in parameters smaller than tolerance, iteration stopped")
+            lambda_1_val_itr, lambda_2_val_itr, lambda_3_val_itr,intensity_1_val_itr,intensity_2_val_itr,intensity_3_val_itr,backgrounds_val_itr = backup[0],backup[1],backup[2],backup[3],backup[4],backup[5],backup[6]
+            break
         lambda_val /= 10
         lambda_1_val_itr, lambda_2_val_itr, lambda_3_val_itr,intensity_1_val_itr,intensity_2_val_itr,intensity_3_val_itr,backgrounds_val_itr= param_list_itr[0],param_list_itr[1],param_list_itr[2],param_list_itr[3],param_list_itr[4],param_list_itr[5],param_list_itr[6]
 
     else:
         lambda_val *= 10
-        param_list_itr = param_list_backup
+        
+        if np.sum(np.absolute(param_diff)) <= tolerance:
+                print("chi_new greater than chi_2, difference in parameters smaller than tolerance, iteration stopped")
+                lambda_1_val_itr, lambda_2_val_itr, lambda_3_val_itr,intensity_1_val_itr,intensity_2_val_itr,intensity_3_val_itr,backgrounds_val_itr = backup[0],backup[1],backup[2],backup[3],backup[4],backup[5],backup[6]
+                param_list_itr = param_list_backup
+                break
         lambda_1_val_itr, lambda_2_val_itr, lambda_3_val_itr,intensity_1_val_itr,intensity_2_val_itr,intensity_3_val_itr,backgrounds_val_itr = backup[0],backup[1],backup[2],backup[3],backup[4],backup[5],backup[6]
-    
+
 
 print(param_list_itr)
+curvature = curvature_matrix(lambda_1_val_itr,lambda_2_val_itr,lambda_3_val_itr,intensity_1_val_itr,intensity_2_val_itr,intensity_3_val_itr,backgrounds_val_itr,t_val)
 covariance = np.linalg.inv(curvature)
-
 print("half life for primary beam is: ", np.log(2)/lambda_1_val_itr , " +-", np.log(2)/(lambda_1_val_itr**2)*np.sqrt(covariance[0][0]),"s" )
 
 if lambda_2_val_init!=0:
@@ -644,7 +678,7 @@ ax1.set_xlabel("time(s)")
 ax1.set_ylabel("Activity")
 
 ################################################################EXPERIMENTAL DATA SCATTERD################################################################################
-ax1.scatter(t_val,decay_counts,color = "red")
+ax1.step(t_val,decay_counts,color = "red",where="mid",linewidth = 1.5)
 ax1.set_xticks(np.arange(0,2,0.16))
 
 ax1.legend()
@@ -672,7 +706,22 @@ ax3.plot(t_val,log_secondary_fit_function_val(lambda_1_val_itr, decay_cons_20Na,
 ax3.set_xlabel("time(s)")
 ax3.set_ylabel("log Activity")
 ax3.set_title("Semi-log Decay Activity Fit")
-
 ax3.legend()
 
-plt.show()
+# print(fit_function_val(lambda_1_val_itr, lambda_2_val_itr, lambda_3_val_itr,t_val,intensity_1_val_itr,intensity_2_val_itr,intensity_3_val_itr,backgrounds_val_itr))
+
+
+
+# print( "the backgrounds is:",np.ones(len(t_val))*dy_dbackgrounds(lambda_1_val_itr, lambda_2_val_itr, lambda_3_val_itr,t_val,intensity_1_val_itr,intensity_2_val_itr,intensity_3_val_itr,backgrounds_val_itr)
+# )
+# print(dy_dlambda_1(lambda_1_val_itr, lambda_2_val_itr, lambda_3_val_itr,t_val,intensity_1_val_itr,intensity_2_val_itr,intensity_3_val_itr,backgrounds_val_itr)
+# )
+# print(dy_dlambda_2(lambda_1_val_itr, lambda_2_val_itr, lambda_3_val_itr,t_val,intensity_1_val_itr,intensity_2_val_itr,intensity_3_val_itr,backgrounds_val_itr)
+# )
+
+
+# print(gradient_val(lambda_1_val_itr, lambda_2_val_itr, lambda_3_val_itr,t_val,intensity_1_val_itr,intensity_2_val_itr,intensity_3_val_itr,backgrounds_val_itr)
+# )
+# print(curvature)
+
+# plt.show()
